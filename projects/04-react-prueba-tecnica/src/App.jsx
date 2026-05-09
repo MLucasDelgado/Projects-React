a42ac20c20ea678fa20c2b05cab4ac595d7af388
MLucasDelgado <lucasdelgado1011@gmail.com>
2026-05-09 17:45:20 -0300

    refactor: simplifica App usando custom hooks y actualización dinámica sin recargar página

--- projects/04-react-prueba-tecnica/src/App.jsx
+++ projects/04-react-prueba-tecnica/src/App.jsx
@@ -1,40 +1,19 @@
-import { useState, useEffect } from 'react'
 import './App.css'
-
-const CAT_ENDPOINT_RANDOM_FACT = 'https://catfact.ninja/fact'
+import { useCat } from './hooks/useCatFact'
+import { useCatImge } from './hooks/useCatImage'
 
 export function App () {
-  const [fact, setFact] = useState()
-  const [imageUrl, setImageUrl] = useState()
+  const { fact, randomFact } = useCat()
+  const { imageUrl } = useCatImge({ fact })
 
-  // Obtener un hecho aleatorio sobre gatos al cargar el componente
-  useEffect(() => {
-    fetch(CAT_ENDPOINT_RANDOM_FACT)
-      .then(res => res.json())
-      .then(data => {
-        const { fact } = data
-        setFact(fact)
-      })
-  }, [])
-
-  // Obtener la imagen del gato usando las primeras tres palabras del hecho
-  useEffect(() => {
-    if (!fact) return
-
-    const firstThreeLetters = fact.split(' ', 3).join(' ')
-
-    fetch(`https://cataas.com/cat/says/${firstThreeLetters}?fontSize=50&fontColor=white&json=true`)
-      .then(res => res.json())
-      .then(response => {
-        const { url } = response
-        setImageUrl(url)
-      })
-  }, [fact])
+  const handleClick = async () => {
+    randomFact()
+  }
 
   return (
     <main>
-      <h1>App de gatitos</h1>
-
+      <h1>Kitten App</h1>
+      <button onClick={handleClick}>Get new fact</button>
       <section>
         {
           fact && <p>{fact}</p>
