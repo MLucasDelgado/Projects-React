0ef8e987c77813f87ef74c7c39c6de002fe42bb3
Lucas Delgado <lucasdelgado1011@gmail.com>
2026-05-12 13:04:20 -0300

    feat: agrega componente WinnerModal

---
+++ projects/02-tic-tac-toe/src/components/WinnerModal.tsx
@@ -0,0 +1,28 @@
+import { Square } from "./Square";
+
+type WinnerModalProps = {
+  winner: string | null | false
+  resetGame: () => void
+}
+
+export function WinnerModal ({ winner, resetGame }: WinnerModalProps) {
+    if (winner === null) return null;
+
+    const winnerText = winner === false ? 'Empate' : 'Ganó: '
+    return (
+        <section className="winner">
+            <div className="text">
+                <h2>{winnerText}</h2>
+                <header className="win">
+                    {
+                        winner && <Square>{winner}</Square>
+                    }
+                </header>
+
+                <footer>
+                    <button onClick={resetGame}>Empezar de nuevo</button>
+                </footer>
+            </div>
+        </section>
+    )
+}
