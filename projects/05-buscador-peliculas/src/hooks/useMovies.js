4b40394ad907b3f3620f06627e769b7346a3d1ba
Lucas Delgado <lucasdelgado1011@gmail.com>
2026-05-14 15:03:24 -0300

    feat: crea hook useMovies para gestión y búsqueda de películas

---
+++ projects/05-buscador-peliculas/src/hooks/useMovies.js
@@ -0,0 +1,33 @@
+import { useState, useRef, useMemo, useCallback } from 'react'
+import { searchMovies } from '../services/movies'
+
+export function useMovies ({ sort }) {
+    const [movies, setMovies] = useState([])
+    const [loading, setLoading] = useState(false)
+    const [error, setError] = useState(null)
+    const previousSearch = useRef()
+
+    const getMovies = useCallback(async ({ search }) => {
+        if(search === previousSearch.current) return
+        try {
+            setLoading(true)
+            setError(null)
+            previousSearch.current = search
+            const newMovies = await searchMovies({ search })
+            setMovies(newMovies)
+        } catch (error) {
+            setError(error.message)
+        } finally {
+            // entra tanto en el try como el catch
+            setLoading(false)
+        }
+    }, [])
+
+    const sortedMovies = useMemo(() => {
+        return sort 
+            ? [...movies].sort((a, b) => a.title.localeCompare(b.title))
+            : movies
+    }, [sort, movies])
+
+    return { movies: sortedMovies, getMovies, loading, error}
+}
