1537516d8c3909e55351893bbde9b784e0968026
MLucasDelgado <lucasdelgado1011@gmail.com>
2026-05-21 00:45:50 -0300

    feat: implementa Footer para visualizar estado del carrito

---
+++ projects/06-shopping-cart/src/components/footer/Footer.jsx
@@ -0,0 +1,17 @@
+import './Footer.css'
+import { useCart } from '../../hooks/useCart';
+
+export function Footer () {
+  // const { filters } = useFilters()
+  const { cart } = useCart()
+
+  return (
+    <footer className='footer'>
+      {
+        JSON.stringify(cart, null, 2)
+      }
+      <h4>Prueba técnica de React ⚛️ － <span>@midudev</span></h4>
+      <h5>Shopping Cart con useContext & useReducer</h5>
+    </footer>
+  )
+}
