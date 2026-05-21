d79a83759043ed1234e3a89d9cab760bdc0c9754
MLucasDelgado <lucasdelgado1011@gmail.com>
2026-05-21 00:45:19 -0300

    feat: implementa filtros por precio y categoría

---
+++ projects/06-shopping-cart/src/components/filters/Filters.jsx
@@ -0,0 +1,56 @@
+import { useId } from 'react'
+import { useFilters } from '../../hooks/useFilters'
+import './Filters.css'
+
+export function Filters ({ products }) {
+    const { filters, setFilters } = useFilters()
+    const minPriceFilterId = useId()
+    const categoryFilterId = useId()
+
+    const categories = [...new Set( // set para eliminar duplicados y luego lo volvemos a convertir en array con el spread operator
+        products?.map(product => product.category) 
+    )]
+
+    const handleChangeMinPrice = (event) => {
+        setFilters(prevState => ({
+            ...prevState,
+            minPrice: (event.target.value)
+        }))
+    }
+
+    const handleChangeCategory = (event) => {
+        setFilters(prevState => ({
+            ...prevState,
+            category: (event.target.value)
+        }))
+    }
+
+    return (
+        <section className='filters'>
+            <div>
+                <label htmlFor={minPriceFilterId}>Minimum Price:</label>
+                <input 
+                    type='range'
+                    id={minPriceFilterId}
+                    min='0'
+                    max='2000'
+                    onChange={handleChangeMinPrice}
+                    value={filters.minPrice}
+                />
+                <span>${filters.minPrice}</span>
+            </div>
+
+            <div>
+                <label htmlFor={categoryFilterId}>Category:</label>
+                <select id={categoryFilterId} onChange={handleChangeCategory} value={filters.category}>
+                    <option value='all'>All</option>
+                    {categories.map(category => (
+                        <option key={category} value={category}>
+                            {category}
+                        </option>
+                    ))}
+                </select>
+            </div>
+        </section>
+    )
+}
