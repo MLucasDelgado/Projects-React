46557c053cc6074fd14c873e830dffc55f5a2778
Lucas Delgado <lucasdelgado1011@gmail.com>
2026-05-21 23:30:33 -0300

    feat(header): agregar titulo e integrar filtros

--- projects/06-shopping-cart/src/components/header/Header.jsx
+++ projects/06-shopping-cart/src/components/header/Header.jsx
@@ -3,7 +3,7 @@ import { Filters } from "../filters/Filters"
 export function Header ({  products }) {
     return (
         <header>
-            <h1>Shopping Cart</h1>
+            <h1>Shopping Cart 🛒</h1>
             <Filters products={products}/>
         </header>
     )
