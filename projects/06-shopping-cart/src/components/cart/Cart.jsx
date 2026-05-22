fa7546174c32cf2c01f19f4ac5d6a4b6d2328825
Lucas Delgado <lucasdelgado1011@gmail.com>
2026-05-21 23:27:01 -0300

    feat(cart): agregar manejo de productos y vaciado del carrito

--- projects/06-shopping-cart/src/components/cart/Cart.jsx
+++ projects/06-shopping-cart/src/components/cart/Cart.jsx
@@ -1,15 +1,48 @@
-import { CartIcon, ClearCartIcon, RemoveFromCartIcon } from '../icons/Icons';
+import { CartIcon, ClearCartIcon } from '../icons/Icons';
 import { useId } from 'react';
+import { useCart } from '../../hooks/useCart'
 import './Cart.css'
 
+function CartItem ({thumbnail, title, price, quantity, addToCart, removeFromCart}) {
+    return (
+        <li>
+            <button className='remove-button' onClick={removeFromCart}>✕</button>
+            <img src={thumbnail} alt={title} />
+            <div>
+                <strong>{title}</strong> - ${price}
+            </div>
+            <footer>
+                <small>
+                    Qty: {quantity}
+                </small>
+                <button onClick={addToCart}>+</button>
+            </footer>
+        </li>
+    )
+}
+
 export function Cart () {
+    const { cart, clearCart, addToCart, removeFromCart } = useCart()
     const cartCheckboxId = useId()
     return (
         <>
             <label className='cart-button' htmlFor={cartCheckboxId}><CartIcon /></label>
             <input id={cartCheckboxId} type="checkbox" hidden />
             <aside className='cart'>
-
+                <ul>
+                    {
+                        cart.map(product => (
+                            <CartItem 
+                            key={product.id} 
+                            addToCart={() => addToCart(product)}
+                            removeFromCart={() => removeFromCart(product)}
+                            {...product}/>
+                        ))
+                    }
+                </ul>
+            <button onClick={clearCart}>
+                <ClearCartIcon />
+            </button>
             </aside>
         </>
     )
