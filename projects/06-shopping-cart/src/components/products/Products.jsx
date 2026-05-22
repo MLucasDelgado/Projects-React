5cf344c76c17c53ff6854b081e5970eea9f13333
Lucas Delgado <lucasdelgado1011@gmail.com>
2026-05-21 23:30:59 -0300

    feat(products): agregar logica para añadir y eliminar productos del carrito

--- projects/06-shopping-cart/src/components/products/Products.jsx
+++ projects/06-shopping-cart/src/components/products/Products.jsx
@@ -1,25 +1,43 @@
-import { AddToCartIcon } from "../icons/Icons";
+import { AddToCartIcon, RemoveFromCartIcon } from "../icons/Icons";
 import { useCart } from "../../hooks/useCart";
-import './Products.css'
+import "./Products.css";
 
-export function Products ({ products }) {
-    const { addToCart } = useCart();
-    return (
-        <main className="products">
-            <ul>
-                {products.map(product => (
-                    <li key={product.id}>
-                        <img src={product.thumbnail} alt={product.title} />
-                        <div>
-                            <strong>{product.title}</strong> - ${product.price}
-                        </div>
-                        <button onClick={() => addToCart(product)}>
-                            <AddToCartIcon />
-                        </button>
-                    </li>
-                ))
-                }
-            </ul>
-        </main>
-    )
+export function Products({ products }) {
+  const { addToCart, removeFromCart, cart } = useCart();
+
+  const checkProductInCart = (product) => {
+    return cart.some((item) => item.id === product.id);
+  };
+
+  return (
+    <main className="products">
+      <ul>
+        {products.map((product) => {
+          const isProductInCart = checkProductInCart(product);
+          return (
+            <li key={product.id}>
+              <img src={product.thumbnail} alt={product.title} />
+              <div>
+                <strong>{product.title}</strong>
+                <span>${product.price}</span>
+              </div>
+
+              <footer>
+                <button
+                  style={{ backgroundColor: isProductInCart ? "red" : "#09f" }}
+                  onClick={() =>
+                    isProductInCart
+                      ? removeFromCart(product)
+                      : addToCart(product)
+                  }
+                >
+                  {isProductInCart ? <RemoveFromCartIcon /> : <AddToCartIcon />}
+                </button>
+              </footer>
+            </li>
+          );
+        })}
+      </ul>
+    </main>
+  );
 }
