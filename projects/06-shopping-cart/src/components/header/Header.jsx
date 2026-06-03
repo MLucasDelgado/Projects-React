155a7bd37e82d0277480304b1655118dbcc9cf0c
MLucasDelgado <lucasdelgado1011@gmail.com>
2026-06-03 01:17:25 -0300

    feat(header): mostrar cantidad de productos en el carrito

--- projects/06-shopping-cart/src/components/header/Header.jsx
+++ projects/06-shopping-cart/src/components/header/Header.jsx
@@ -1,10 +1,24 @@
-import { Filters } from "../filters/Filters"
+import './Header.css'
+import { CartIcon } from '../icons/Icons'
+import { useCart } from '../../hooks/useCart'
 
-export function Header ({  products }) {
-    return (
-        <header>
-            <h1>Shopping Cart 🛒</h1>
-            <Filters products={products}/>
-        </header>
-    )
+export function Header({ isCartOpen, setIsCartOpen }) {
+  const { cart } = useCart()
+
+  const totalItems = cart.reduce((acc, item) => {
+    return acc + item.quantity
+  }, 0)
+
+  return (
+    <header className='header'>
+      <h1 className='header-title'>
+        SHOPPING <span className='header-span'>CART</span>
+      </h1>
+
+      <button className='header-cart-info' onClick={() => setIsCartOpen(!isCartOpen)}>
+        <CartIcon />
+        <span>{totalItems}</span>
+      </button>
+    </header>
+  )
 }
