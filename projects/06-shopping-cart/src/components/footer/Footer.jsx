c4910f255ca90821c1e67004a9ca7f3ec42b7f9d
MLucasDelgado <lucasdelgado1011@gmail.com>
2026-06-03 01:16:27 -0300

    feat(footer): agregar secciones informativas y enlaces en el pie de página

--- projects/06-shopping-cart/src/components/footer/Footer.jsx
+++ projects/06-shopping-cart/src/components/footer/Footer.jsx
@@ -1,17 +1,58 @@
 import './Footer.css'
-// import { useCart } from '../../hooks/useCart';
+import {
+  FaGithub ,
+  FaInstagram,
+  FaLinkedinIn
+} from 'react-icons/fa'
 
-export function Footer () {
-  // const { filters } = useFilters()
-  // const { cart } = useCart()
-
+export function Footer() {
   return (
     <footer className='footer'>
-      {/* {
-        JSON.stringify(cart, null, 2)
-      } */}
-       <p>Prueba técnica de React ⚛️ — <span>@midudev</span></p>
-      <small>Shopping Cart con useContext & useReducer</small>
+      <div className='footer-content'>
+        <div className='footer-brand'>
+          <h2>ShoppingCart.</h2>
+
+          <p>
+            Discover quality products at the best prices.
+            Shop comfortably from anywhere.
+          </p>
+
+          <div className='social-icons'>
+            <a target='_blank' rel='noopener noreferrer' href='https://github.com/MLucasDelgado'><FaGithub /></a>
+            <a target='_blank' rel='noopener noreferrer' href='https://www.instagram.com/lucas_mdelgado/'><FaInstagram /></a>
+            <a target='_blank' rel='noopener noreferrer' href='https://www.linkedin.com/in/lucas-delgado-77b582165/'><FaLinkedinIn /></a>
+          </div>
+        </div>
+
+        <div className='footer-links'>
+          <div>
+            <h4>Company</h4>
+            <a href='#'>About us</a>
+            <a href='#'>Services</a>
+            <a href='#'>Blog</a>
+          </div>
+
+          <div>
+            <h4>Support</h4>
+            <a href='#'>Help Center</a>
+            <a href='#'>FAQ</a>
+            <a href='#'>Contact</a>
+          </div>
+
+          <div>
+            <h4>Legal</h4>
+            <a href='#'>Privacy</a>
+            <a href='#'>Terms</a>
+            <a href='#'>Cookies</a>
+          </div>
+        </div>
+      </div>
+
+      <div className='footer-bottom'>
+        <p>
+          Copyright © 2026. All Rights Reserved.
+        </p>
+      </div>
     </footer>
   )
 }
