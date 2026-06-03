76fdac83a43cf9822265f81f9c9de1dd0b6b12e9
MLucasDelgado <lucasdelgado1011@gmail.com>
2026-06-03 01:20:20 -0300

    feat(cart): integrar apertura y cierre del carrito desde la aplicación

--- projects/06-shopping-cart/src/App.jsx
+++ projects/06-shopping-cart/src/App.jsx
@@ -1,25 +1,36 @@
-import { products as initialProducts } from './mocks/products.json'
-import { Products } from './components/products/Products'
-import { Header } from './components/header/Header'
-import { useState } from 'react'
-import { Footer } from './components/footer/Footer'
-import { useFilters } from './hooks/useFilters'
-import { Cart } from './components/cart/Cart'
-import { CartProvider } from './context/CartProvider'
+import { products as initialProducts } from "./mocks/products.json";
+import { Products } from "./components/products/Products";
+import { Header } from "./components/header/Header";
+import { useState } from "react";
+import { Footer } from "./components/footer/Footer";
+import { useFilters } from "./hooks/useFilters";
+import { Cart } from "./components/cart/Cart";
+import { CartProvider } from "./context/CartProvider";
+import { Filters } from "./components/filters/Filters";
+import "./index.css";
 
 function App() {
-  const [products] = useState(initialProducts)
-  const { filterProducts } = useFilters()
+  const [products] = useState(initialProducts);
+  const [isCartOpen, setIsCartOpen] = useState(false);
+  const { filterProducts } = useFilters();
 
-  const filteredProducts = filterProducts(products)
+  const filteredProducts = filterProducts(products);
   return (
     <CartProvider>
-      <Cart />
-      <Header products={products} />
-      <Products products={filteredProducts} />
-      <Footer />
+      <div className="app">
+        <Header isCartOpen={isCartOpen} setIsCartOpen={setIsCartOpen} />
+
+        <main className="main-content">
+          <Filters products={products} />
+          <Products products={filteredProducts} />
+        </main>
+
+        <Cart isCartOpen={isCartOpen} setIsCartOpen={setIsCartOpen} />
+
+        <Footer />
+      </div>
     </CartProvider>
-  )
+  );
 }
 
-export default App
+export default App;
